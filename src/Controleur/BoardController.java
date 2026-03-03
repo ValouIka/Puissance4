c8a7a827ea7454633985acfe7dca48063b3f39f9
valentin dauge <lili66-90@hotmail.fr>
2026-03-03 22:54:36 +0100

    Ajustements

--- src/Controleur/BoardController.java
+++ src/Controleur/BoardController.java
@@ -6,54 +6,78 @@ import Modele.Board;
 import Vue.BoardDisplay;
 import Vue.MainMenu;
 
-public class BoardController extends MouseAdapter{
+public class BoardController extends MouseAdapter {
 
     private Board board;
     private BoardDisplay display;
     private boolean over = false;
 
-    public BoardController(Board board, BoardDisplay display){
+    public BoardController(Board board, BoardDisplay display) {
         this.board = board;
         this.display = display;
         display.addMouseListener(this);
     }
 
     @Override
-    public void mouseClicked(MouseEvent e){
-        if(over){
+    public void mouseClicked(MouseEvent e) {
+        if (over) {
             return;
         }
 
-        int col = e.getX()/100;
-        if(col < 0 || col >= board.col){
+        int col = e.getX() / 100;
+        if (col < 0 || col >= board.col) {
             return;
         }
 
-        if(board.getPlayersNumber() == 0){
-            while(board.checkWin() == 0){
-                board.addTokenAlea();
+        // Mode 0 joueur : IA contre IA
+        if (board.getPlayersNumber() == 0) {
+            while (board.checkWin() == 0) {
+                int bestCol = board.getBestMove(6);
+                board.addToken(bestCol);
+                // Après chaque coup, vérifier la victoire
+                byte result = board.checkWin();
+                if (result != 0) {
+                    over = true;
+                    JOptionPane.showMessageDialog(display, "Victoire du joueur " + result + " !");
+                    break;
+                }
+                // Changer de joueur manuellement
+                board.player = (byte) (board.player == 1 ? 2 : 1);
+            }
+            display.repaint();
+            return;
+        }
+
+// Gestion des tours pour 1 ou 2 joueurs
+        boolean joueurAJoue = false;
+
+// Cas : ordinateur doit jouer (mode 1 joueur et c'est le tour de l'ordi)
+        if (board.getPlayersNumber() == 1 && board.player == 2) {
+            int bestCol = board.getBestMove(8);
+            board.addToken(bestCol);
+            joueurAJoue = true;
+        }
+// Cas : joueur humain peut jouer (mode 2 joueurs, ou mode 1 joueur et c'est le tour de l'humain)
+        else if (board.getPlayersNumber() == 2 || (board.getPlayersNumber() == 1 && board.player == 1)) {
+            board.addToken(col);
+            joueurAJoue = true;
+        }
+
+        if (joueurAJoue) {
+            // Vérifier la victoire
+            byte result = board.checkWin();
+            if (result != 0) {
+                over = true;
+            } else {
+                // Pas de victoire : passer au joueur suivant
+                board.player = (byte) (board.player == 1 ? 2 : 1);
+            }
+            display.repaint();
+            if(over){
+                JOptionPane.showMessageDialog(display, "Victoire du joueur " + result + " !");
             }
         }
 
-        //Joue automatiquement si c'est au tour de l'ordinateur
-        if((board.getPlayersNumber() == 1 && board.player == 2)){
-            board.addTokenAlea();
-        }
-
-        //Permet au joueur actuel de jouer
-        else if((board.getPlayersNumber() == 2) || (board.getPlayersNumber() == 1 && board.player == 1)){
-            board.addToken(col);
-        }
-        byte result = board.checkWin();
-        display.repaint();
-        if(result != 0){
-            System.out.println("test");
-            over = true;
-            JOptionPane.showMessageDialog(display,
-                    "Victoire du joueur "+result+" !");
-        }
-
-
-
     }
+
 }
--- src/Modele/Board.java
+++ src/Modele/Board.java
@@ -15,15 +15,13 @@ public class Board {
         this.player = 1;
     }
 
-    public void addToken(int colNumber){
-        for(int r = row-1; r >= 0; r--){
-            if(board[r][colNumber] == 0){
+    public void addToken(int colNumber) {
+        for (int r = row - 1; r >= 0; r--) {
+            if (board[r][colNumber] == 0) {
                 board[r][colNumber] = this.player;
-                this.player = (byte)(player == 1 ? 2 : 1);
                 break;
             }
         }
-        //Sinon renvoyer exception pas de place?
     }
 
     public void addTokenAlea(){
@@ -112,5 +110,183 @@ public class Board {
     public void setPlayersNumber(byte pn){
         this.playersNumber = pn;
     }
+
+
+    private boolean isValidMove(int c) {
+        return c >= 0 && c < this.col && this.board[0][c] == 0;
+    }
+
+    private boolean isBoardFull() {
+        for (int c = 0; c < this.col; c++) {
+            if (this.board[0][c] == 0) return false;
+        }
+        return true;
+    }
+
+    private int makeTempMove(int c, byte player) {
+        for (int r = this.row - 1; r >= 0; r--) {
+            if (this.board[r][c] == 0) {
+                this.board[r][c] = player;
+                return r;
+            }
+        }
+        return -1;
+    }
+
+    private void undoTempMove(int r, int c, byte player) {
+        this.board[r][c] = 0;
+    }
+
+    private byte checkWinFrom(int row, int col) {
+        byte p = this.board[row][col];
+        if (p == 0 || p == 3) return 0;
+
+        int[][] dirs = {{0,1},{1,0},{1,1},{1,-1}};
+        for (int[] d : dirs) {
+            int count = countDir(row, col, d[0], d[1], p) +
+                    countDir(row, col, -d[0], -d[1], p) - 1;
+            if (count >= 4) return p;
+        }
+        return 0;
+    }
+
+    private int countDir(int r, int c, int dr, int dc, byte p) {
+        int count = 1;
+        int nr = r + dr, nc = c + dc;
+        while (nr >= 0 && nr < this.row && nc >= 0 && nc < this.col &&
+                this.board[nr][nc] == p) {
+            count++;
+            nr += dr; nc += dc;
+        }
+        return count;
+    }
+
+    public int getBestMove(int depth) {
+        int bestScore = Integer.MIN_VALUE;
+        int bestCol = this.col / 2;
+        byte opponent = (byte)(this.player == 1 ? 2 : 1);
+
+        // 1. Victoire immédiate
+        for (int c = 0; c < this.col; c++) {
+            if (isValidMove(c)) {
+                int rowPlaced = makeTempMove(c, this.player);
+                if (checkWinFrom(rowPlaced, c) == this.player) {
+                    undoTempMove(rowPlaced, c, this.player);
+                    return c;
+                }
+                undoTempMove(rowPlaced, c, this.player);
+            }
+        }
+
+        // 2. Blocage adversaire
+        for (int c = 0; c < this.col; c++) {
+            if (isValidMove(c)) {
+                int rowPlaced = makeTempMove(c, opponent);
+                if (checkWinFrom(rowPlaced, c) == opponent) {
+                    undoTempMove(rowPlaced, c, opponent);
+                    return c;
+                }
+                undoTempMove(rowPlaced, c, opponent);
+            }
+        }
+
+        //minimax
+        for (int c = 0; c < this.col; c++) {
+            if (isValidMove(c)) {
+                int rowPlaced = makeTempMove(c, this.player);
+                int score = minimax(rowPlaced, c, depth-1, Integer.MIN_VALUE,
+                        Integer.MAX_VALUE, false, this.player);
+                undoTempMove(rowPlaced, c, this.player);
+                System.out.printf("Col %d: score=%d\n", c, score);
+                if (score > bestScore) {
+                    bestScore = score;
+                    bestCol = c;
+                }
+            }
+        }
+        return bestCol;
+    }
+
+    private int minimax(int lastRow, int lastCol, int depth, int alpha, int beta,
+                        boolean isMaxPlayer, byte aiPlayer) {
+        byte winner = checkWinFrom(lastRow, lastCol);
+        if (winner != 0) {
+            return (winner == aiPlayer) ? 10000-depth*100 : depth*100-10000;
+        }
+        if (depth == 0 || isBoardFull()) {
+            return evaluateBoard(aiPlayer);
+        }
+
+        byte currentPlayer = isMaxPlayer ? aiPlayer : (byte)(aiPlayer == 1 ? 2 : 1);
+
+        if (isMaxPlayer) {
+            int maxEval = Integer.MIN_VALUE;
+            for (int c = 0; c < this.col; c++) {
+                if (isValidMove(c)) {
+                    int r = makeTempMove(c, currentPlayer);
+                    int score = minimax(r, c, depth-1, alpha, beta, false, aiPlayer);
+                    undoTempMove(r, c, currentPlayer);
+                    maxEval = Math.max(maxEval, score);
+                    alpha = Math.max(alpha, score);
+                    if (beta <= alpha) break;
+                }
+            }
+            return maxEval;
+        } else {
+            int minEval = Integer.MAX_VALUE;
+            for (int c = 0; c < this.col; c++) {
+                if (isValidMove(c)) {
+                    int r = makeTempMove(c, currentPlayer);
+                    int score = minimax(r, c, depth-1, alpha, beta, true, aiPlayer);
+                    undoTempMove(r, c, currentPlayer);
+                    minEval = Math.min(minEval, score);
+                    beta = Math.min(beta, score);
+                    if (beta <= alpha) break;
+                }
+            }
+            return minEval;
+        }
+    }
+
+    private int evaluateBoard(byte aiPlayer) {
+        byte opp = (byte)(aiPlayer == 1 ? 2 : 1);
+        return countAlignments(3, aiPlayer)*5000 - countAlignments(3, opp)*5000 +
+                countAlignments(2, aiPlayer)*200 - countAlignments(2, opp)*200 +
+                getCenterScore(aiPlayer)*50;
+    }
+
+    private int countAlignments(int len, byte player) {
+        int count = 0;
+        // Horizontal
+        for (int r = 0; r < this.row; r++)
+            for (int c = 0; c <= this.col-len; c++)
+                if (checkLine(r, c, 0, 1, len, player)) count++;
+        // Vertical
+        for (int c = 0; c < this.col; c++)
+            for (int r = 0; r <= this.row-len; r++)
+                if (checkLine(r, c, 1, 0, len, player)) count++;
+        return count;
+    }
+
+    private boolean checkLine(int r, int c, int dr, int dc, int len, byte player) {
+        for (int i = 0; i < len; i++) {
+            int nr = r + i*dr, nc = c + i*dc;
+            if (nr < 0 || nr >= this.row || nc < 0 || nc >= this.col ||
+                    this.board[nr][nc] != player)
+                return false;
+        }
+        return true;
+    }
+
+    private int getCenterScore(byte player) {
+        int score = 0, center = this.col/2;
+        for (int r = 0; r < this.row; r++)
+            if (this.board[r][center] == player) score++;
+        return score;
+    }
+
+
+
+
 
 }
